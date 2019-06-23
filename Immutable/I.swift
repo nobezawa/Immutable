11b26495ed9853d46086c2eb62a02a39c0ff3a04
nobezawa <charmy.nobezawa@gmail.com>
2019-06-23 17:56:45 +0900

    add I.list.swift

--- Immutable/I.swift
+++ Immutable/I.swift
@@ -23,37 +23,4 @@ open class I {
     public static func equals<T: Equatable>(_ lhs: T) -> (_ to: T) -> Bool {
         return curry(equals)(lhs)
     }
-
-    public static func map<A, B>(_ function: (A) -> B, _ functor: [A]) -> [B] {
-        return functor.map(function)
-    }
-
-    public static func map<A, B>(_ function: @escaping (A) -> B) -> (_ with: [A]) -> [B] {
-        return curry(map)(function)
-    }
-
-    public static func append<A>(_ lhs: A, _ rhs: [A]) -> [A] {
-        return rhs + [lhs]
-    }
-
-    public static func append<A>(_ lhs: A) -> (_ with: [A]) -> [A] {
-        return curry(append)(lhs)
-    }
-
-    public static func first<A>(_ elements: [A]) -> Optional<A> {
-        return elements.first
-    }
-
-    public static func last<A>(_ elements: [A]) -> Optional<A> {
-        return elements.last
-    }
-
-    public static func take<A>(_ xtake: Int, _ elements: [A]) -> ArraySlice<A> {
-        if elements.isEmpty || xtake == 0 { return [] }
-        return elements.prefix(xtake)
-    }
-
-    public static func take<A>(_ xtake: Int) -> (_ elements: [A]) -> ArraySlice<A> {
-        return curry(take)(xtake)
-    }
 }
